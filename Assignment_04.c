6c21b27d72d52161998bed6b7d5dad7af391ee1c
feldbacherSimone <simone.feldbacher@gmail.com>
2026-03-14 16:21:55 +0100

    finish thread 2 and 3 implemntation

--- Assignment_04.c
+++ Assignment_04.c
@@ -5,11 +5,25 @@
 #include <time.h>
 #include <unistd.h>
 #include <math.h>
+#include "aes.h" 
+#include <string.h>
 
 #define FLASH_SIZE (1024 * 1024)
+static uint8_t flash_segement[FLASH_SIZE];
 
-#define T_SIZE1 3000
-#define T_SIZE2 6000
+
+static const uint8_t aes_key[16] = {
+    0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+    0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+};
+
+static const uint8_t aes_iv[16] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+};
+
+#define T_SIZE1 6000
+#define T_SIZE2 8000
 
 typedef enum 
 {
@@ -54,7 +68,7 @@ static int determineL(int window[], int size)
 static void processSample(int t1[], int size1, int t2[], int size2, int *deltaV, int *L)
 {
     int currentSample = rand() % (10 + 1 + 30) - 30;
-    printf("Current Sample: %d\n", currentSample);
+    //printf("Current Sample: %d\n", currentSample);
 
     slideWindow(t1, size1, currentSample);
     slideWindow(t2, size2, currentSample);
@@ -62,8 +76,9 @@ static void processSample(int t1[], int size1, int t2[], int size2, int *deltaV,
     *deltaV = determineDeltaV(t1, size1);
     *L = determineL(t2, size2);
 
-    printf("DeltaV: %d, L: %d\n", *deltaV, *L);
+    //printf("DeltaV: %d, L: %d\n", *deltaV, *L);
 }
+
 
 void* thread_airbag(void* arg) {
 
@@ -89,44 +104,44 @@ int t1[T_SIZE1] = {0};
         switch (state)
         {
         case NORMAL:
-            printf("NORMAL\n");
+            //printf("NORMAL\n");
             processSample(t1, T_SIZE1, t2, T_SIZE2, &deltaV, &L);
 
             if (abs(deltaV) > deltaV1)
             {
-                printf("go to STANDBY\n");
+                //printf("go to STANDBY\n");
                 state = STANDBY;
             }
             break;
 
         case STANDBY:
-           printf("STANDBY\n");
+           //printf("STANDBY\n");
             processSample(t1, T_SIZE1, t2, T_SIZE2, &deltaV, &L);
             if (abs(deltaV) > deltaV2 || L > LThreshold)
             {
-                printf("go to FIRE\n");
+                //printf("go to FIRE\n");
                 state = FIRE;
             }
             if (abs(deltaV) < deltaV1)
             {
-                printf("go to NORMAL\n");
+                //printf("go to NORMAL\n");
                 state = NORMAL;
             }
             break;
 
         case FIRE:
-            printf("FIRE AIRBAG\n");
+            //printf("FIRE AIRBAG\n");
             break;
 
         default:
-            printf("Unknown State\n");
+            //printf("Unknown State\n");
             break;
         }
         
 
         // Advance deadline by period
 
-        next.tv_nsec += 1000000000; // 1 ms
+        next.tv_nsec += 1000000; // 1 ms
         if(next.tv_nsec >= 1000000000)
         {
             next.tv_sec += 1;
@@ -138,17 +153,59 @@ int t1[T_SIZE1] = {0};
     return NULL;
 }
 
+uint16_t crc16(const uint8_t* data, size_t length){
+    uint16_t crc = 0xFFFF; 
+    for(size_t i = 0; i < length; i++){
+        crc ^= (uint16_t)data[i] << 8; 
+
+        for(int j = 0; j < 8; j++){
+            if(crc & 0x8000){
+                crc = (crc<< 1) ^ 0x1021; 
+            }
+            else {
+                crc <<= 1; 
+            }
+        }
+    }
+    return crc; 
+}
+
+void encrypt_crc(uint16_t crc, uint8_t *out, size_t *out_len){
+
+    struct AES_ctx ctx; 
+
+    out[0] = (crc >> 8) & 0xFF;
+    out[1] = crc & 0xFF;  
+
+    AES_init_ctx_iv(&ctx, aes_key, aes_iv);
+
+    AES_CTR_xcrypt_buffer(&ctx, out, 2);
+
+    *out_len = 2; 
+}
+
 void* thread_checksum(void* arg) {
-    static uint8_t flash_buffer[FLASH_SIZE];
+    memset(flash_segement, 0xA5, FLASH_SIZE);
+
 
     struct timespec next; 
     clock_gettime(CLOCK_MONOTONIC, &next);
     for(;;)
     {
-        (void)flash_buffer[0];
-        printf("Checking checksum...\n");
+        printf("Getting checksum...\n");
 
-        next.tv_nsec += 100000000; // 10 ms
+        uint16_t crc = crc16(flash_segement, FLASH_SIZE);
+
+        uint8_t crc_encrypted[2]; 
+        size_t encrypted_len = 0; 
+
+        encrypt_crc(crc, crc_encrypted, &encrypted_len);
+
+        // simulate send 
+        printf("CRC16: 0x%04X, Encrypted CRC: 0x%02X%02X\n", crc, crc_encrypted[0], crc_encrypted[1]);
+
+        //timing
+        next.tv_nsec += 500000000; // 500 ms
         if(next.tv_nsec >= 1000000000)
         {
             next.tv_sec += 1;
@@ -164,8 +221,29 @@ void* thread_diagnostic(void* arg) {
     clock_gettime(CLOCK_MONOTONIC, &next);
     for(;;)
     {
+        // simulate data reciving 
+        uint8_t recived[2]; 
+        memset(recived, 0x42, sizeof(recived));
+
+        // decrypt with AES 
+        
+        struct AES_ctx ctx_in; 
+
+        AES_init_ctx_iv(&ctx_in, aes_key, aes_iv);
+        AES_CTR_xcrypt_buffer(&ctx_in, recived, sizeof(recived));
+
+        // status 
+        uint8_t status[2] = {0x10, 0x11};
+        struct AES_ctx ctx_out; 
+        AES_init_ctx_iv(&ctx_out, aes_key, aes_iv);
+        AES_CTR_xcrypt_buffer(&ctx_out, status, sizeof(status));
+
+        // simulate send 
+        printf("Sending status: 0x%02X%02x\n", status[0], status[1]);
+
+        //timing
         printf("Running diagnostics...\n");
-        next.tv_nsec += 500000000; // 5 ms
+        next.tv_nsec += 100000000; // 100 ms
         if(next.tv_nsec >= 1000000000)
         {
             next.tv_sec += 1;
@@ -177,13 +255,19 @@ void* thread_diagnostic(void* arg) {
 }
 
 int main() {
-    pthread_t airbag_thread;
-    //pthread_t checksum_thread;
-    //pthread_t diagnostic_thread;
 
+    pthread_t airbag_thread;
     pthread_create(&airbag_thread, NULL, thread_airbag, NULL);
-    //pthread_create(&checksum_thread, NULL, thread_checksum, NULL);
-    //pthread_create(&diagnostic_thread, NULL, thread_diagnostic, NULL);
+    pthread_setname_np(airbag_thread, "airbag");
+
+    pthread_t checksum_thread;
+    pthread_create(&checksum_thread, NULL, thread_checksum, NULL);
+    pthread_setname_np(checksum_thread, "checksum"); 
+
+
+    pthread_t diagnostic_thread;
+    pthread_create(&diagnostic_thread, NULL, thread_diagnostic, NULL);
+    pthread_setname_np(diagnostic_thread, "diagnostics");
 
     for(;;); 
     return EXIT_SUCCESS;
